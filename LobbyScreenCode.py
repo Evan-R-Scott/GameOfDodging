c61934cf796394847394d04bc5b5a618b244f5a9
Evan12Scott <evan12scott@gmail.com>
2023-06-28 03:08:05 -0700

    lobby screen

--- LobbyScreenCode.py
+++ LobbyScreenCode.py
@@ -1,13 +1,33 @@
 #lobby screen
 import pygame
+
+pygame.init()
+
 class Button:
     def __init__(self, x,  y, image, scale):
-        self.x = x
-        self.y = y
+
+        width = image.get_width()
+        height = image.get_height()
         self.scale = scale
-        self.image = image
+        self.image = pygame.transform.scale(image, int(width * scale), int(height * scale))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
     
-    def lobbyScreen(self):
+    def lobbyScreen(self, window):
+        
+        inAction = False
+
+        mousePos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mousePos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                inAction = True
+
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
         check = False
 
         if check:
--- Python Pygame Dodge Game Project.py
+++ Python Pygame Dodge Game Project.py
@@ -1,5 +1,5 @@
 import pygame
-##import Button
+import LobbyScreenCode
 #main loop code
 pygame.init()
 
@@ -8,10 +8,8 @@ screen_height = 500
 screen_width = 800
 
 background_size = (screen_width, screen_height)
+topLeft = (0,0)
 
-#button calculations
-start_locX = screen_width / 2 - screen_width
-start_locY = screen_height * 1/3
 menuState = "main"
 
 #menu background
@@ -21,10 +19,13 @@ menu_background = pygame.transform.scale(menu_background,background_size)
 #button
 start_button = pygame.image.load("start_button-removebg-preview.png")
 difficulty_button = pygame.image.load("difficulty_button.png")
-map_button = pygame.image.load("map_button.jfif")
-back_button = pygame.image.load("back_button.jfif")
+map_button = pygame.image.load("map_button-removebg-preview.png")
+back_button = pygame.image.load("back_button-removebg-preview.png")
 
-
+start_img = LobbyScreenCode.Button(50, 50, start_button, 0.5)
+difficulty_img = LobbyScreenCode.Button(50, 50, difficulty_button, 0.5)
+map_img = LobbyScreenCode.Button(50, 50, map_button, 0.5)
+back_img = LobbyScreenCode.Button(50, 50, back_button, 0.5)
 
 #winter game background
 winter_game_background = pygame.image.load("winter_game_background.jpg")
@@ -48,15 +49,26 @@ pygame.display.set_caption("Dodge Game")
 #game loop
 run = True
 while run:
+
+    
     if menuState == "main":
-        screen.blit(menu_background,(0,0))
-        screen.blit(start_button,(start_locX , start_locY))
+        screen.blit(menu_background,topLeft)
+
+        start_img.lobbyScreen(screen)
+        difficulty_img.lobbyScreen(screen)
+        map_img.lobbyScreen(screen)
+        back_img.lobbyScreen(screen)
+        
     elif menuState == "game":
-        screen.blit(grassy_game_background, (0,0))
+        screen.blit(grassy_game_background, topLeft)
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_TAB:
+                menuState = "main"
+                break
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
                 menuState = "game"
                 break
         if event.type == pygame.QUIT:
